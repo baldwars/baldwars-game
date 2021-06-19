5f9fbf0acad7a1df37df6e8a80fb521b80a6c1de
Antonin Texier <antonin.texier@gmail.com>
2021-06-19 21:21:17 +0200

    Rename prototypes function.

--- src/ai_tools/tools.h
+++ src/ai_tools/tools.h
@@ -8,9 +8,9 @@ Cell *get_cell();
 Cell *get_cell_of(Warrior *);
 
 // MOVEMENTS
-size_t move_toward(Cell *);
-size_t move_toward_with_moves(Cell *, size_t);
-size_t move_away_from(Cell *);
-size_t move_away_from_with_moves(Cell *, size_t);
+size_t move_toward_enemy(Cell *);
+size_t move_toward_enemy_with_moves(Cell *, size_t);
+size_t move_away_from_enemy(Cell *);
+size_t move_away_from_enemy_with_moves(Cell *, size_t);
 
 #endif //BALDWARS_GAME_TOOLS_H
