41bf60699b7ffc96c23fdc36e6c37729a7d761e2
Antonin Texier <antonin.texier@gmail.com>
2021-06-19 21:20:03 +0200

    Add function to get cell of a warrior.

--- src/ai_tools/tools.c
+++ src/ai_tools/tools.c
@@ -1,1 +1,13 @@
 #include "tools.h"
+
+// CELLS
+Cell *get_cell()
+{
+    Warrior *warrior = get_current_warrior();
+    return warrior->cell;
+}
+
+Cell *get_cell_of(Warrior *warrior)
+{
+    return warrior->cell;
+}
