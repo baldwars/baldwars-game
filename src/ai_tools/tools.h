f42c61dc101caa818a4bc57cdaf23dece2e3c575
Antonin Texier <antonin.texier@gmail.com>
2021-06-19 20:57:31 +0200

    Add cells and movements prototype functions.

--- src/ai_tools/tools.h
+++ src/ai_tools/tools.h
@@ -3,6 +3,13 @@
 
 #include "../game/game.h"
 
+// CELLS
+Cell *get_cell_of(Warrior *);
 
+// MOVEMENTS
+size_t move_toward(Cell *);
+size_t move_toward_with_moves(Cell *, size_t);
+size_t move_away_from(Cell *);
+size_t move_away_from_with_moves(Cell *, size_t);
 
 #endif //BALDWARS_GAME_TOOLS_H
