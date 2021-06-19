a31c7e6799919b2b00281389c71cddb9c690307a
Antonin Texier <antonin.texier@gmail.com>
2021-06-19 20:42:46 +0200

    Add accessors prototypes.

--- src/game/game.h
+++ src/game/game.h
@@ -3,17 +3,20 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <cJSON.h>
 #include "game_struct.h"
 
 #define MAP_SIZE 20
+#define WALLS_MIN_RATIO 0.2
+#define WALLS_MAX_RATIO 0.4
 
 typedef struct cell_t Cell;
 typedef struct weapon_t Weapon_;
 typedef struct warrior_t Warrior;
 
 struct cell_t {
-    unsigned int x;
-    unsigned int y;
+    size_t x;
+    size_t y;
 };
 
 struct weapon_t {
@@ -27,23 +30,24 @@ struct weapon_t {
 struct warrior_t {
     unsigned short id;
     const char *name;
-    unsigned int level;
-    unsigned int health;
-    unsigned int moves;
-    unsigned int actions;
+    size_t level;
+    size_t health;
+    size_t moves;
+    size_t actions;
     Weapon_ *weapon;
 };
 
 // INIT
 Cell *cell_init(int, int);
 Weapon_ *weapon_init();
-Warrior *warrior_init(unsigned short, const char *, unsigned int, unsigned int, unsigned int, unsigned int);
+Warrior *warrior_init(unsigned short, const char *, size_t, size_t, size_t, size_t);
 int **map_init();
 
 void game_start();
 int **generate_random_map();
 void generate_walls_on_map(int ***);
 void locate_warriors_on_map(int ***, Warrior **, size_t);
+Warrior *load_warrior(cJSON *);
 Warrior **load_warriors(size_t *);
 
 void initMap (int index);
@@ -57,6 +61,12 @@ bool inRange (Character *target);
 void print_map(int **);
 void print_warrior(Warrior *);
 void print_warriors(Warrior **, size_t);
+
+// ACCESSORS
+int **get_map();
+Warrior **get_warriors();
+Warrior *get_current_warrior();
+size_t get_current_round();
 
 
 #endif //BALDWARS_GAME_GAME_FUNCTION_H
