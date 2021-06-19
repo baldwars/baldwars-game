260dbb00079617204d3d14a88ac0090a27e7c919
Antonin Texier <antonin.texier@gmail.com>
2021-06-19 15:45:13 +0200

    Get user from json file and generate random map.

--- src/game/game.c
+++ src/game/game.c
@@ -239,3 +239,177 @@ void writeJSON (char *string) {
     fprintf(jsonFile, string);
     fclose(jsonFile);
 }
+
+Cell *cell_init(int x, int y)
+{
+    Cell *cell = malloc(sizeof(Cell));
+    *cell = (Cell){
+        .x = x,
+        .y = y
+    };
+
+    return cell;
+}
+
+Warrior *warrior_init(unsigned short id,const char *name, unsigned int level,
+                          unsigned int health, unsigned int moves, unsigned int action)
+{
+    Warrior *warrior = malloc(sizeof(Warrior));
+    *warrior = (Warrior){
+        .id = id,
+        .name = name,
+        .level = level,
+        .health = health,
+        .actions = action,
+        .moves = moves,
+        .weapon = NULL
+    };
+
+    return warrior;
+}
+
+Warrior **load_warriors(size_t *warriors_length)
+{
+    char *buffer = get_file_content("./warriors.json");
+
+    cJSON *parsed = cJSON_Parse(buffer);
+    cJSON *item, *id, *name, *level, *health, *moves, *actions;
+
+    *warriors_length = cJSON_GetArraySize(parsed);
+    Warrior **warriors = malloc(sizeof(Warrior *) * (*warriors_length));
+
+    for (int i = 0; i < *warriors_length; ++i)
+    {
+        item = cJSON_GetArrayItem(parsed, i);
+        id = cJSON_GetObjectItemCaseSensitive(item, "id");
+        name = cJSON_GetObjectItemCaseSensitive(item, "name");
+        level = cJSON_GetObjectItemCaseSensitive(item, "level");
+        health = cJSON_GetObjectItemCaseSensitive(item, "health");
+        moves = cJSON_GetObjectItemCaseSensitive(item, "moves");
+        actions = cJSON_GetObjectItemCaseSensitive(item, "actions");
+
+        char *name_val = cJSON_GetStringValue(name);
+        unsigned int id_val = (unsigned int)cJSON_GetNumberValue(id);
+        unsigned int level_val = (unsigned int)cJSON_GetNumberValue(level);
+        unsigned int health_val = (unsigned int)cJSON_GetNumberValue(health);
+        unsigned int actions_val = (unsigned int)cJSON_GetNumberValue(actions);
+        unsigned int moves_val = (unsigned int)cJSON_GetNumberValue(moves);
+
+        warriors[i] = warrior_init(id_val, name_val, level_val, health_val, moves_val, actions_val);
+    }
+    return warriors;
+}
+
+void game_start()
+{
+    size_t warriors_number = 0;
+    Warrior **warriors = load_warriors(&warriors_number);
+    int **map = generate_random_map();
+    locate_warriors_on_map(&map, warriors, warriors_number);
+    print_map(map);
+
+    unsigned int round = 1;
+    unsigned short fight_is_over = 0;
+
+    while (!fight_is_over) {
+        printf("Round %u:\n", round);
+        print_warriors(warriors, warriors_number);
+
+        if (++round > 5) {
+            fight_is_over = 1;
+        }
+    }
+}
+
+int **map_init()
+{
+    int **map = malloc(sizeof(int*) * MAP_SIZE);
+    for (int i = 0; i < MAP_SIZE; ++i) {
+        map[i] = malloc(sizeof(int) * MAP_SIZE);
+    }
+
+    return map;
+}
+
+int **generate_random_map()
+{
+    int **map = map_init();
+
+    for (int i = 0; i < MAP_SIZE; ++i) {
+        for (int j = 0; j < MAP_SIZE; ++j) {
+            map[i][j] = 0;
+        }
+    }
+
+    generate_walls_on_map(&map);
+
+    return map;
+}
+
+void generate_walls_on_map(int ***map)
+{
+    size_t min = MAP_SIZE * MAP_SIZE * 0.2;
+    size_t max = MAP_SIZE * MAP_SIZE * 0.4;
+    size_t walls_number = rand() % (max - min + 1) + min;
+
+    for (int i = 0; i < walls_number; ++i) {
+        size_t random_x, random_y;
+
+        do {
+            random_x = rand() % MAP_SIZE;
+            random_y = rand() % MAP_SIZE;
+        } while ((*map)[random_x][random_y] == -1);
+
+        (*map)[random_x][random_y] = -1;
+    }
+}
+
+void locate_warriors_on_map(int *** map, Warrior **warriors, size_t warriors_number)
+{
+    size_t random_x, random_y;
+
+    for (int i = 0; i < warriors_number; ++i) {
+        do {
+            if (i % 2 == 0) {
+                random_y = rand() % MAP_SIZE / 2;
+            }
+            else {
+                size_t min = MAP_SIZE / 2;
+                size_t max = MAP_SIZE;
+                random_y = rand() % (max - min) + min;
+            }
+            random_x = rand() % MAP_SIZE;
+        } while ((*map)[random_x][random_y] == -1);
+
+        (*map)[random_x][random_y] = warriors[i]->id;
+    }
+}
+
+void print_map(int **map)
+{
+    printf("Map:\n");
+    for (int i = 0; i < MAP_SIZE; ++i) {
+        for (int j = 0; j < MAP_SIZE; ++j) {
+            printf("%d ", map[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void print_warrior(Warrior *warrior)
+{
+    printf("%s:\n", warrior->name);
+    printf("  id: %u\n", warrior->id);
+    printf("  level: %u\n", warrior->level);
+    printf("  health: %u\n", warrior->health);
+    printf("  moves: %u\n", warrior->moves);
+    printf("  actions: %u\n\n", warrior->actions);
+}
+
+void print_warriors(Warrior **warriors, size_t length)
+{
+    for (int i = 0; i < length; ++i) {
+        print_warrior(warriors[i]);
+    }
+}
